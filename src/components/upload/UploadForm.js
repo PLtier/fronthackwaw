6d223de1e2a38d2f89afc11a604f62282c44660f
Your Name <maciej.jalocha.pl@gmail.com>
2019-10-12 20:39:52 +0200

    added map settings to upload

--- src/components/upload/UploadForm.js
+++ src/components/upload/UploadForm.js
@@ -1,11 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import SimpleMap from "./Map";
 
 class Form extends Component {
-  state = { title: "", email: "", description: "", prize: 0 };
+  state = { title: "", email: "", description: "", prize: 0, coordinates: [] };
 
   handleChange = evt => {
     this.setState({ [evt.target.name]: evt.target.value });
   };
+
+  getCoordinates = coordinates =>{
+    this.setState({
+      coordinates: coordinates
+    })
+  }
 
   handleSubmit = event => {
     event.preventDefault();
@@ -21,30 +28,33 @@ class Form extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Imię:
-          <input type="text" name="title" onChange={this.handleChange} />
-        </label>
-        <label>
-          Email
-          <input type="text" name="email" onChange={this.handleChange} />
-        </label>
-        <label>
-          Description
-          <textarea
-            rows="4"
-            cols="50"
-            name="description"
-            onChange={this.handleChange}
-          ></textarea>
-        </label>
-        <label>
-          Prize
-          <input type="number" name="prize" onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Wyślij" />
-      </form>
+      <Fragment>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Imię:
+            <input type="text" name="title" onChange={this.handleChange} />
+          </label>
+          <label>
+            Email
+            <input type="text" name="email" onChange={this.handleChange} />
+          </label>
+          <label>
+            Description
+            <textarea
+              rows="4"
+              cols="50"
+              name="description"
+              onChange={this.handleChange}
+            ></textarea>
+          </label>
+          <label>
+            Prize
+            <input type="number" name="prize" onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Wyślij" />
+        </form>
+        <SimpleMap getCoordinates={this.getCoordinates}/>
+      </Fragment>
     );
   }
 }
